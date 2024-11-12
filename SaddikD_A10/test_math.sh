42bc6d85726be8e04d0c0be70a688d5da6a15bc9
deemasaddik <deema.saddik@gmail.com>
2024-11-11 19:51:36 -0800

    Adding A10

---
+++ SaddikD_A10/test_math.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+python3 <<EOF
+
+from math import power, divide
+import unittest
+class TestMathFunction(unittest.TestCase):
+
+  def test_power(self):
+      self.assertEqual(power(-2,3), -8)
+      self.assertEqual(power(3,-2), 1/9)
+      self.assertEqual(power(3,0), 1)
+      self.assertEqual(power(0,19999), 0)
+      
+
+  def test_divide(self):
+    self.assertEqual(divide(-2,8), -0.25)
+    self.assertEqual(divide(0,8), 0)
+    with self.assertRaises(ZeroDivisionError):
+        divide(8, 0)
+
+if __name__ == '__main__':  #  Python scripts are importable as modules or standalone
+      unittest.main()
+EOF
+
