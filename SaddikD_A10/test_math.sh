b5a08f732d1a0497e91844bf0df0a50d5053cb18
deemasaddik <deema.saddik@gmail.com>
2024-11-11 20:01:54 -0800

    Updating A10

--- SaddikD_A10/test_math.sh
+++ SaddikD_A10/test_math.sh
@@ -11,10 +11,15 @@ class TestMathFunction(unittest.TestCase):
       self.assertEqual(power(3,-2), 1/9)
       self.assertEqual(power(3,0), 1)
       self.assertEqual(power(0,19999), 0)
-      
+      self.assertEqual(power(9,2),81)
+      self.assertEqual(power(9,1/2), 3)
+      self.assertEqual(power(1/9,1/2), 1/3)
 
   def test_divide(self):
     self.assertEqual(divide(-2,8), -0.25)
+    self.assertEqual(divide(-2,-8), 0.25)
+    self.assertEqual(divide(2,-8), -0.25)
+    self.assertEqual(divide(50,10), 5)
     self.assertEqual(divide(0,8), 0)
     with self.assertRaises(ZeroDivisionError):
         divide(8, 0)
