4f55a863209e6bc9a793ffa621fd40333c7cbaea
deemasaddik <deema.saddik@gmail.com>
2024-12-05 16:04:03 -0800

    fixed bug, adding A12

--- crime_test/setup.py
+++ crime_test/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='crime_test',
-    version="3.12.7",
+    version="1.0",
     packages=find_packages(),
     install_requires=[
         "pandas",
