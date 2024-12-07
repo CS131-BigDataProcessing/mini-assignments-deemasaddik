499719ac063dee114485fa3e0c6147c7253b734a
deemasaddik <80138103+deemasaddik@users.noreply.github.com>
2024-12-06 16:46:11 -0800

    Delete crime_test directory

--- crime_test/__init__.py
+++
@@ -1,5 +0,0 @@
-print("Welcome to the crime test")
-#Python 3.12.7
-__version__ = "3.12.7"
-__author__ = "Deema Saddik"
-
--- crime_test/stats_function.py
+++
@@ -1,5 +0,0 @@
-def calculate_mean(column):
-    return column.mean()
-
-def calculate_median(column):
-    return column.median()
